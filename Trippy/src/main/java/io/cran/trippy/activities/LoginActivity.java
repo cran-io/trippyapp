e1a2571dbc27ac8450293385fcb3804bde9c627f
solrubado <sol@cran.io>
2016-03-11 17:57:23 -0300

    Sign up new user

--- Trippy/src/main/java/io/cran/trippy/activities/LoginActivity.java
+++ Trippy/src/main/java/io/cran/trippy/activities/LoginActivity.java
@@ -1,7 +1,10 @@
 package io.cran.trippy.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 import com.parse.ParseObject;
 
@@ -16,10 +19,14 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
-        ParseObject testObject = new ParseObject("TestObject");
-        testObject.put("foo", "bar");
-        testObject.saveInBackground();
+        TextView signUpLink= (TextView) findViewById(R.id.signUpLink);
+        signUpLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i= new Intent(LoginActivity.this, SignUpActivity.class);
+                startActivity(i);
+            }
+        });
 
 
 
--- Trippy/src/main/java/io/cran/trippy/activities/SignUpActivity.java
+++ Trippy/src/main/java/io/cran/trippy/activities/SignUpActivity.java
@@ -1,15 +1,70 @@
 package io.cran.trippy.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 import io.cran.trippy.R;
 
 public class SignUpActivity extends AppCompatActivity {
 
+    protected EditText mFirstName;
+    protected EditText mLastName;
+    protected EditText mEmailAddress;
+    protected EditText mBirthDate;
+    protected EditText mPassword;
+    protected ImageView mSignUpBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        mFirstName= (EditText) findViewById(R.id.firstNameET);
+        mLastName=(EditText) findViewById(R.id.lastNameET);
+        mEmailAddress=(EditText) findViewById(R.id.emailAddressET);
+        mBirthDate=(EditText) findViewById(R.id.birthDate);
+        mPassword=(EditText)findViewById(R.id.password);
+        mSignUpBtn= (ImageView) findViewById(R.id.signUpBtn);
+
+        mSignUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String firstName= mFirstName.getText().toString().trim();
+                String lastName= mLastName.getText().toString().trim();
+                String emailAddress= mEmailAddress.getText().toString().trim();
+                String birthDate= mBirthDate.getText().toString().trim();
+                String password= mPassword.getText().toString().trim();
+
+                ParseUser newUser= new ParseUser();
+                newUser.setUsername(firstName);
+                newUser.put("lastname",lastName);
+                newUser.setEmail(emailAddress);
+                newUser.put("birthdate",birthDate);
+                newUser.setPassword(password);
+                newUser.signUpInBackground(new SignUpCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        if(e==null){
+                            Intent i= new Intent(SignUpActivity.this,MainActivity.class);
+                            startActivity(i);
+                        }
+                        else{
+                            Toast.makeText(SignUpActivity.this,""+e.getMessage(),Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+            }
+        });
     }
 }
