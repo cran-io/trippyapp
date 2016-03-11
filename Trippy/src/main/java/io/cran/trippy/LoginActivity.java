4ab8bbe81ceb9a73d290ae36253faca4ee651806
solrubado <sol@cran.io>
2016-03-10 21:17:19 -0300

    Login Layout

---
+++ Trippy/src/main/java/io/cran/trippy/LoginActivity.java
@@ -0,0 +1,24 @@
+package io.cran.trippy;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * A login screen that offers login via email/password.
+ */
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+
+    }
+
+
+    }
+
+
+
